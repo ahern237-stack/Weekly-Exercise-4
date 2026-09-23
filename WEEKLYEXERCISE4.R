c69fdd99d4ae1f8c94c56d0e02341b2b55ee038c
Abigail <Ahern237@uic.edu>
2026-09-23 16:54:50 -0500

    #a

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -4,3 +4,15 @@ default_branch <- git_branch()
 print(default_branch)
 
 #Default branch is "main"
+
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+Olympics <- read_csv("Olympics.csv")
+
+#a
+Olympics <- Olympics %>%
+  mutate(total.medals = gold + silver + bronze)
+
