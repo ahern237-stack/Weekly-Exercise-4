e8b5dcc20a2d312a660fdd2ee3f88306751a91bd
Abigail <Ahern237@uic.edu>
2026-09-23 17:33:25 -0500

    #7.a.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -4,3 +4,12 @@ default_branch <- git_branch()
 print(default_branch)
 
 #Default branch is "main"
+
+#7.a.
+largest_delegation_1992 <- Olympics %>%
+  filter(year == 1992) %>% select(country, athletes)
+
+print(largest_delegation_1992)
+
+#Largest delegation was the United states with 147 athletes, the second largest
+#delegation was the Unified Team (Former Soviet).
